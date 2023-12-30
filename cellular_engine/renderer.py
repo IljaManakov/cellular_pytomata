0e3a5bbba4155aab57dca4d12ebb2741d32f99f2
Ilja Manakov <ilja.manakov@gmx.de>
2023-12-30 18:21:41 +0100

    Initial commit

---
+++ cellular_engine/renderer.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Renderer:
+
+    def __init__(self, engine):
+        self.engine = engine
+        self.engine_iterator = iter(engine)
+        self.paused = True
+        self.figure, self.axes = plt.subplots(1, 1, figsize=(6, 6))
+        self.canvas = self.figure.canvas
+        self.timer = self.canvas.new_timer(interval=1)
+        self.background = None
+
+        grid_shape = engine.grid.shape
+        x, y = np.meshgrid(range(grid_shape[0]), range(grid_shape[1]))
+        self.points = self.axes.scatter(x, y, marker='s')
+        self.points.set_array(engine.grid.flat)
+
+        self.axes.get_xaxis().set_visible(False)
+        self.axes.get_yaxis().set_visible(False)
+        self.axes.set_facecolor('black')
+        self.figure.tight_layout()
+
+        self.timer.add_callback(self._run_step)
+        self.canvas.mpl_connect('key_press_event', self._handle_key_press)
+        self.canvas.mpl_connect('draw_event', lambda event: setattr(self, 'background', None))
+
+    def start(self):
+        self.timer.start()
+        plt.show()
+
+    def _run_step(self):
+        if self.paused:
+            return
+
+        self.canvas.restore_region(self.canvas.copy_from_bbox(self.axes.bbox))
+        step, grid = next(self.engine_iterator)
+        self.points.set_array(grid.flat)
+        self.axes.draw_artist(self.points)
+        self.figure.canvas.blit(self.axes.bbox)
+        self.canvas.flush_events()
+
+    def _handle_key_press(self, event):
+        if event.key == ' ':
+            self.paused = not self.paused
+
+        if event.key == 'q':
+            plt.close()
---
+++ game_of_life.py
@@ -0,0 +1,22 @@
+import numpy as np
+from matplotlib import pyplot
+
+from cellular_engine import Engine
+from cellular_engine.renderer import Renderer
+
+
+def overpopulation(neighbourhood: np.ndarray) -> float | None:
+    return 0. if neighbourhood.sum() > 3 else None
+
+
+def reproduction(neighbourhood: np.ndarray) -> float | None:
+    return 1. if neighbourhood.sum() == 3 else None
+
+
+def underpopulation(neighbourhood: np.ndarray) -> float | None:
+    return 0. if neighbourhood.sum() < 2 else None
+
+
+engine = Engine(np.random.randint(0, 2, (64, 64)), [overpopulation, reproduction, underpopulation])
+renderer = Renderer(engine)
+renderer.start()
